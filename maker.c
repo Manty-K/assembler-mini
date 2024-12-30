4480a3fb4daffdcd2f0ce68dc4c19cda66ca9cf0
Manthan Khandale <manthankhandale@gmail.com>
2024-12-30 14:59:14 +0530

    help added

--- maker.c
+++ maker.c
@@ -18,10 +18,10 @@ void makeeverything(char *file)
 {
     char command[100];
 
-    system("flex p.l");
-    system("bison -d p.y --warnings=none");
-    system("gcc objgen.c -o objgen.o");
-    system("gcc p.tab.c lex.yy.c utils.c symb.c modrm.c -lfl -o p.o -w");
+    // system("flex p.l");
+    // system("bison -d p.y --warnings=none");
+    // system("gcc objgen.c -o objgen.o");
+    // system("gcc p.tab.c lex.yy.c utils.c symb.c modrm.c -lfl -o p.o -w");
 
     bzero(command, sizeof(command));
     sprintf(command, "./p.o 1 < %s > /dev/null && ./p.o 2 < %s > output.lst", file, file);
@@ -60,74 +60,105 @@ int main(int argc, char const *argv[])
     strcpy(filename, argv[1]);
 
     int option = 0;
+    int isAll = 0;
 
     if (argc > 2)
     {
-
-        if (!strcmp("-s", argv[2]) || !strcmp("-s", argv[2]))
+        if (!strcmp("-all", argv[2]))
         {
-            option = 1; // Symbol Table
-        }
-        else if (!strcmp("-l", argv[2]))
-        {
-            option = 2; // Listing File
-        }
-
-        else if (!strcmp("-o", argv[2]))
-        {
-            option = 0; // Object File
-        }
-        else if (!strcmp("-D", argv[2]) || !strcmp("-d", argv[2]))
-        {
-            option = 3; // Show Data setion
-        }
-        else if (!strcmp("-T", argv[2]) || !strcmp("-t", argv[2]))
-        {
-            option = 4; // Show Data setion
-        }
-        else if (!strcmp("-L", argv[2]))
-        {
-            option = 5; // Show LST file
+            isAll = 1;
         }
         else
         {
-            printf("Invalid Argument %s\n", argv[2]);
-            puts("Valid Argumants:\n-o: Object Code (Default)\n-s,-S: Symbol Table\n-d,-D Data Section\n-t,-T: Text Section");
 
-            exit(1);
+            if (!strcmp("-s", argv[2]) || !strcmp("-s", argv[2]))
+            {
+                option = 1; // Symbol Table
+            }
+            else if (!strcmp("-l", argv[2]))
+            {
+                option = 2; // Listing File
+            }
+
+            else if (!strcmp("-o", argv[2]))
+            {
+                option = 0; // Object File
+            }
+            else if (!strcmp("-D", argv[2]) || !strcmp("-d", argv[2]))
+            {
+                option = 3; // Show Data setion
+            }
+            else if (!strcmp("-T", argv[2]) || !strcmp("-t", argv[2]))
+            {
+                option = 4; // Show Data setion
+            }
+            else if (!strcmp("-L", argv[2]))
+            {
+                option = 5; // Show LST file
+            }
+            else
+            {
+                if (strcmp("-h", argv[2]))
+                {
+                    printf("Invalid Argument: %s\n", argv[2]);
+                }
+                printf("\nUsage: masm filename [option]\n");
+                printf("\nOptions\n");
+
+                puts("-o\tObject Code (Default)\n-s,-S\tSymbol Table\n-d,-D\tData Section\n-t,-T\tText Section\n-l,-L\tListing File\n-all\tShow All");
+
+                exit(1);
+            }
         }
     }
     makeeverything(filename);
 
-    switch (option)
+    if (isAll)
     {
-    case 0: // Object Code
-        system("cat output.o");
-        break;
-    case 1: // Symbol Table
+        printf("Symbol Table\n");
         system("cat p.sym");
-        break;
-    case 2: // Listing File
-        system("cat output.lst");
-        break;
-    case 3: // Data Section
-        system("awk '/SECTION DATA/ {found=1} found' output.lst | sed '1d' | awk '/SECTION/ {exit} {print}' | grep .");
-        break;
-    case 4: // Text Section
-        system("awk '/SECTION TEXT/ {found=1} found' output.lst | sed '1d' | tr -d '[,' |tr -d '],'| grep .");
-        break;
-    case 5: // List file advance
+
+        printf("\nListing File\n");
         char command[300];
         memset(command, 0, sizeof(command));
         sprintf(command, "paste output.lst %s | nl", filename);
         system(command);
+        printf("\nObject File\n");
+        system("xxd output.o");
+    }
+    else
+    {
 
-        break;
+        switch (option)
+        {
+        case 0: // Object Code
+            system("cat output.o");
+            break;
+        case 1: // Symbol Table
+            system("cat p.sym");
+            break;
+        case 2: // Listing File
+            system("cat output.lst");
+            break;
+        case 3: // Data Section
+            system("awk '/SECTION DATA/ {found=1} found' output.lst | sed '1d' | awk '/SECTION/ {exit} {print}' | grep .");
+            break;
+        case 4: // Text Section
+            system("awk '/SECTION TEXT/ {found=1} found' output.lst | sed '1d' | tr -d '[,' |tr -d '],'| grep .");
+            break;
+        case 5: // Listing file advance
+            char command[300];
+            memset(command, 0, sizeof(command));
+            sprintf(command, "paste output.lst %s | nl", filename);
+            system(command);
 
-    default:
-        puts("Invalid option");
-        exit(1);
-        break;
+            break;
+
+        default:
+            puts("Invalid option");
+            exit(1);
+            break;
+        }
     }
 
     return 0;
